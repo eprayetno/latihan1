70154a187eadda652526ac66d3731503b4c848c3
Eko Hadi Prayetno <echo.prayetno@gmail.com>
2020-03-19 23:51:30 +0700

    Tugas2: HTML dalam PHP

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
     <tittle>Belajar PHP</tittle>
 </head>
 <body>
-    <h1>Selamat datang <?php echo "Eko Hadi Prayetno" ?></h1>
+ <?php
+echo "<h1>Selamat datang, Eko Hadi Prayetno</h1>";
+ ?>
 </body>
 </html>
