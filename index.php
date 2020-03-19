9de169228dbe266cbdf692f7ab9b6ad8da255e4c
Eko Hadi Prayetno <echo.prayetno@gmail.com>
2020-03-20 00:45:30 +0700

    Tugas7: Operator assignment

--- index.php
+++ index.php
@@ -4,10 +4,29 @@
 </head>
 <body>
 <?php
-$nama_depan = "Eko";
-$nama_belakang = "Prayetno";
+$x = 10;
+$x += 2;
+echo $x."</br>";
 
-echo $nama_depan." ".$nama_belakang;
+$y = 10;
+$y -= 2;
+echo $y."</br>";
+
+$z = 10;
+$z *= 2;
+echo $z."</br>";
+
+$a = 10;
+$a /= 2;
+echo $a."</br>";
+
+$b = 10;
+$b %= 2;
+echo $b."</br>";
+
+$c = 10;
+$c .= 2;
+echo $c;
 ?>
 </body>
 </html>
