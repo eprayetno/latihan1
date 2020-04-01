cc79d22b62d7ec0bfb3d5ab86f6304a915b57bd8
Eko Hadi Prayetno <echo.prayetno@gmail.com>
2020-04-01 19:45:07 +0700

    minggu4 Task_10

--- minggu4/latihan3.php
+++ minggu4/latihan3.php
@@ -1,10 +1,16 @@
 <?php
-$x=20;
-if ($x<10) {
-    echo "benar";
-} elseif ($x==20) {
-    echo "BINGGO!";
+$nilai=87;
+if ($nilai<50) {
+    echo "nilai E";
+} elseif ($nilai>=50 && $nilai<60) {
+    echo "nilai D";
+} elseif ($nilai>=60 && $nilai<70) {
+    echo "nilai C";
+} elseif ($nilai>=70 && $nilai<80) {
+    echo "nilai B";
+} elseif ($nilai>=80 && $nilai<90) {
+    echo "nilai A";
 } else {
-    echo "salah";
+    echo "nilai A+";
 }
 ?>
