8d8715ab5c102a1617c94c88dd181c6aa1cfbddf
Eko Hadi Prayetno <echo.prayetno@gmail.com>
2020-04-11 20:57:08 +0700

    Tugas Function Task8

---
+++ minggu5/fungsi.php
@@ -0,0 +1,9 @@
+<?php
+function perkalian($angka1,$angka2) {
+    return $angka1*$angka2;
+}
+$hasil = perkalian(20,3);
+echo "Perkalian 20x3 adalah $hasil";
+echo "<br>";
+echo "Perkalian 9x2 adalah ".perkalian(9,2);
+?>
