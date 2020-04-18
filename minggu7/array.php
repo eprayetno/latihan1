e66f379a95b42711c25b06b32c33ccf9075e866f
Eko Hadi Prayetno <echo.prayetno@gmail.com>
2020-04-18 18:08:58 +0700

    tugas array task1

---
+++ minggu7/array.php
@@ -0,0 +1,14 @@
+<?php
+$hari = array("senin","selasa","rabu");
+
+var_dump($hari);
+echo "</br>";
+echo $hari[0];
+echo "</br>";
+print_r($hari);
+echo "</br>";
+
+$hari[] = "kamis";
+print_r($hari);
+
+?>
